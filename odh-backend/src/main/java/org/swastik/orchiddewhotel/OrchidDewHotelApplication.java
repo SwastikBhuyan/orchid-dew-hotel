55423415559be520f6e1ea3bf0471cb9cacbf4de
SwastikBhuyan <154882312+SwastikBhuyan@users.noreply.github.com>
2024-07-24 20:13:57 +0530

    Initial commit

---
+++ odh-backend/src/main/java/org/swastik/orchiddewhotel/OrchidDewHotelApplication.java
@@ -0,0 +1,13 @@
+package org.swastik.orchiddewhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchidDewHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchidDewHotelApplication.class, args);
+	}
+
+}
---
+++ odh-backend/src/test/java/org/swastik/orchiddewhotel/OrchidDewHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swastik.orchiddewhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchidDewHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
