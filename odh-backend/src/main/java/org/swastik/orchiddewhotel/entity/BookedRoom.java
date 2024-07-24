9dde51ad635e76002051469f7a579ae7e829342c
SwastikBhuyan <154882312+SwastikBhuyan@users.noreply.github.com>
2024-07-25 01:38:26 +0530

    Added entity classes

--- odh-backend/src/main/java/org/swastik/orchiddewhotel/entity/BookedRoom.java
+++ odh-backend/src/main/java/org/swastik/orchiddewhotel/entity/BookedRoom.java
@@ -1,4 +1,53 @@
 package org.swastik.orchiddewhotel.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+    @Column(name = "Guest_FullName")
+    private String guestFullName;
+    @Column(name = "Guest_Email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int numOfAdults;
+    @Column(name = "children")
+    private int numOfChildren;
+    @Column(name = "total_guests")
+    private int totalNumOfGuests;
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_Id")
+    private Room room;
+
+    public void calculateTotalNumOfGuests() {
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
+
+
